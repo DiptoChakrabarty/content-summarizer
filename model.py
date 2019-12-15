cd5e97e1728a93810ae1cbd920ebcde4969c6306
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2019-12-15 10:40:18 +0530

    added context search

--- model.py
+++ model.py
@@ -6,12 +6,35 @@ from nltk.stem import SnowballStemmer
 import re
 from nltk.corpus import wordnet
 
-#en = spacy.load('en')
+def context_search(words):
+    synonymns=[]
+    #words=input("give your text : ")
+    for syn in wordnet.synsets(words):
+        for l in syn.lemmas():
+            synonymns.append(l.name())
+    synonymns=set(synonymns)
+    print(synonymns) 
+    file=open('terms.txt','r')
+    content=file.read()
+#print(content)
+    content=nltk.sent_tokenize(content)
+#print(content)
+    sentences=[]
+    for i in range(len(content)):
+        review=re.sub('[^a-zA-Z]',' ',content[i])
+        review=review.lower()
+        review=review.split()
+        review=[word for word in review if word not in set(stopwords.words("english"))]    
+        review= " ".join(review)
+        if len(review)>5:
+            sentences.append(review)
+    context=[]
+    for sent in sentences:
+        for word in synonymns:
+            if word in sent:
+                context.append(sent)
+    return context
 
-synonymns=[]
-words=input("give your text : ")
-for syn in wordnet.synsets(words):
-    for l in syn.lemmas():
-        synonymns.append(l.name())
-synonymns=set(synonymns)
-print(synonymns) 
+word=input("Give word : ")
+print(context_search(word))
+
